9f0e9c152ba09853396e70fcd87b9137ae2f98d1
fromdetail <adiletkadyrov@gmail.com>
2022-07-17 22:37:03 +0600

    feat: add markdown shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,28 @@
+let markdown = require("markdown-it")({
+  html: true,
+});
+
 module.exports = function (eleventyConfig) {
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy({
     "./src/admin/config.yml": "./admin/config.yml",
-    
-    './node_modules/alpinejs/dist/cdn.min.js': './assets/js/alpine.js',
+
+    "./node_modules/alpinejs/dist/cdn.min.js": "./assets/js/alpine.js",
   });
 
   // Copy Image Folder to /_site
   eleventyConfig.addPassthroughCopy("./src/assets/img");
 
+  eleventyConfig.addNunjucksShortcode(
+    "markdown",
+    (content) => `<div class="md-block">${markdown.render(content)}</div>`
+  );
+
   return {
     dir: {
-      input: 'src',
-      includes: '_includes',
-      layouts: '_layouts',
+      input: "src",
+      includes: "_includes",
+      layouts: "_layouts",
     },
   };
 };
