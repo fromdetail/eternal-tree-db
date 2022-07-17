488082441c653faf63694331fa9f57217bd85667
fromdetail <adiletkadyrov@gmail.com>
2022-07-17 16:36:32 +0600

    feat: add temp chara images to characters list

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,12 @@ module.exports = function (eleventyConfig) {
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy({
     "./src/admin/config.yml": "./admin/config.yml",
+    
     './node_modules/alpinejs/dist/cdn.min.js': './assets/js/alpine.js',
   });
+
+  // Copy Image Folder to /_site
+  eleventyConfig.addPassthroughCopy("./src/assets/img");
 
   return {
     dir: {
